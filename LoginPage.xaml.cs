cc6437be32b5b63742e579623c6676318850f45f
Lense04 <allen.vincent0440@gmail.com>
2025-06-17 20:26:49 +0800

    Add project files.

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,10 @@
+
+namespace UM_Consultation_App_MAUI;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/Authentication/CreateAccountPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace UM_Consultation_App_MAUI.Views;
+
+public partial class CreateAccountPage : ContentPage
+{
+	public CreateAccountPage()
+	{
+		InitializeComponent();
+	}
+}
