1b86e9d075df74aed211d18064642a752d8204ed
itsyocoder <pjbayta@gmail.com>
2025-08-25 11:58:43 +0800

    - Added functionality to ConsultationListPage & RequestListPage - new ViewModel primarily for all pages in FacultyView (FacultyRequestViewModel) - ConsultationList and RequestList are both using FacultyRequestViewModel and are binded together (check MauiProgram.cs)

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.Extensions.Logging;
 using CommunityToolkit.Maui;
+using UM_Consultation_App_MAUI.ViewModels;
+using UM_Consultation_App_MAUI.Views.FacultyView;
 
 namespace UM_Consultation_App_MAUI
 {
+
     public static class MauiProgram
     {
+
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -13,6 +17,13 @@ namespace UM_Consultation_App_MAUI
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             }).UseMauiCommunityToolkit();
+
+            // in order for both pages in faculty to be binded by one viewmodel (RequestListViewModel)
+            builder.Services.AddSingleton<FacultyRequestViewModel>();
+            builder.Services.AddSingleton<RequestListPage>();
+            builder.Services.AddSingleton<ConsultationListPage>();
+            // end
+
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
--- Views/FacultyView/ConsultationListPage.xaml.cs
+++ Views/FacultyView/ConsultationListPage.xaml.cs
@@ -1,9 +1,12 @@
+using UM_Consultation_App_MAUI.ViewModels;
+
 namespace UM_Consultation_App_MAUI.Views.FacultyView;
 
 public partial class ConsultationListPage : ContentPage
 {
-	public ConsultationListPage()
+	public ConsultationListPage(FacultyRequestViewModel vm)
 	{
 		InitializeComponent();
-	}
+		BindingContext = vm;
+    }
 }
